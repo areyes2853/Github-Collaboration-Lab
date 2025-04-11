1b54074d0a8291e8f220b32dd8bd6166f467adfe
areyes2853 <46231827+areyes2853@users.noreply.github.com>
2025-04-11 15:08:54 -0400

    adding History to the App.jsx file

--- simple-git-hub-app/src/App.jsx
+++ simple-git-hub-app/src/App.jsx
@@ -3,12 +3,14 @@ import reactLogo from './assets/react.svg'
 import viteLogo from '/vite.svg'
 import Contact from './components/contactUs'
 import Welcome from './components/welcome'
+import HistoryOfOrganisation from './components/history'
 import './App.css'
 
 const App = () => {
   return (
     <>
     <Welcome />
+    <HistoryOfOrganisation />
     <Contact />
     </>
   )
