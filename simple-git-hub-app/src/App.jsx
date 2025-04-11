db72548fa2e51f4fd2cea9f8ec7f3b7a01126e55
areyes2853 <46231827+areyes2853@users.noreply.github.com>
2025-04-11 15:04:23 -0400

    moving the positon of App.css import location

--- simple-git-hub-app/src/App.jsx
+++ simple-git-hub-app/src/App.jsx
@@ -2,8 +2,8 @@ import { useState } from 'react'
 import reactLogo from './assets/react.svg'
 import viteLogo from '/vite.svg'
 import Contact from './components/contactUs'
-import './App.css'
 import Welcome from './components/welcome'
+import './App.css'
 
 const App = () => {
   return (
