e67b53aa1f79146eb32df764dbffa6addc5379c5
Rachel1731 <rbarcellona@tampabay.rr.com>
2025-04-11 14:57:27 -0400

    update

--- simple-git-hub-app/src/App.jsx
+++ simple-git-hub-app/src/App.jsx
@@ -1,7 +1,7 @@
 import { useState } from 'react'
 import reactLogo from './assets/react.svg'
 import viteLogo from '/vite.svg'
-
+import Contact from './components/contactUs'
 import './App.css'
 import Welcome from './components/welcome'
 
@@ -9,7 +9,7 @@ const App = () => {
   return (
     <>
     <Welcome />
-    
+    <Contact />
     </>
   )
 };
