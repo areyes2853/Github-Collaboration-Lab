6ed2587c75d7cd4ea94c0f39ececb890465eb821
DianaWilson1 <shedteckned@gmail.com>
2025-04-11 15:01:21 -0400

    .

--- simple-git-hub-app/src/App.jsx
+++ simple-git-hub-app/src/App.jsx
@@ -3,12 +3,13 @@ import reactLogo from './assets/react.svg'
 import viteLogo from '/vite.svg'
 import './App.css'
 import Welcome from './components/welcome'
+import History from './components/history'
+
 
 const App = () => {
   return (
     <>
-    <Welcome />
-    
+      <Welcome />
     </>
   )
 };
